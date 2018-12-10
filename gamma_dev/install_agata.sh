a9d0f37d1212ab74a5104f0b34c332c9c34a8e04
Guillaume Baulieu <g.baulieu@ipnl.in2p3.fr>
2018-12-10 16:21:06 +0100

    Directly set the environment variables in the docker images

--- gamma_dev/install_agata.sh
+++ gamma_dev/install_agata.sh
@@ -4,7 +4,6 @@ cd AgataSoftware
 git clone https://gitlab.in2p3.fr/IPNL_GAMMA/scripts.git
 python scripts/gRaySoftware.py --cmake="-DCMAKE_BUILD_TYPE=release -- -j 4" --adf= all
 python scripts/gRaySoftware.py --cmake="-DCMAKE_BUILD_TYPE=release -- -j 4" --mfm= all
-source /etc/profile.d/agata_env.sh
 python scripts/gRaySoftware.py --cmake="-DCMAKE_BUILD_TYPE=release -- -j 4" --agapro= all
 python scripts/gRaySoftware.py --cmake="-DCMAKE_BUILD_TYPE=release -- -j 4" --ganpro= all
 python scripts/gRaySoftware.py --cmake="-DCMAKE_BUILD_TYPE=release -- -j 4" --gw= all
